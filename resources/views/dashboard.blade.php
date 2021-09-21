0202ba36f331073a704806ad8e891a947efe35c2
unknown <66fatihavci@gmail.com>
2021-09-21 12:47:47 +0300

    jet brains kurulumu

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <x-slot name="header">
+        Anasayfa
+    </x-slot>
+
+     <x-slot name="message2">
+        <div class="alert alert-danger">ssa</div><!-- layout/app.blade.php deki message2 isimli slot içerisine gönderilir -->
+    </x-slot>
+</x-app-layout>
